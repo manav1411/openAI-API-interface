c5f07b0a53469602bc16701b6031d076ba47a5b5
Manav Dodia <manav@Manavs-MacBook-Air.local>
2023-11-06 06:42:40 +1100

    added main python script

---
+++ gpt4.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+API_KEY = "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"
+API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+
+def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}",
+    }
+
+    data = {
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+    }
+
+    if max_tokens is not None:
+        data["max_tokens"] = max_tokens
+
+    print("gpt4 is generating response...")
+    response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        raise Exception(f"Error {response.status_code}: {response.text}")
+
+#takes in the users input
+user_input = []
+print("your gpt4 prompt: ")
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    user_input.append(line)
+concatenated_input = "\n".join(user_input)
+
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": concatenated_input}
+]
+
+response_text = generate_chat_completion(messages)
+print("\ngpt4 response: ")
+print(response_text)
